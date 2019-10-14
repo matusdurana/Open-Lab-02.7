694deb51caf54adbd3478f8cdd6b8eb089362220
z18ista03 <z18ista03@Z00P1-10.SPSKNM>
2019-10-14 10:01:28 +0200

    Add project files.

---
+++ Open-Lab-02.7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Open_Lab_02._7
+{
+    class Program
+    {
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        {
+            return (month == 12 && day == 24);
+
+        }
+        static void Main(string[] args)
+        {
+            int day;
+            int month;
+            int year;
+           
+            Console.WriteLine("Zadaj deň:");
+            day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadaj Mesiac:");
+            month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadaj rok:");
+            year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(IsTimeForMilkAndCookies(year, month, day));
+            
+
+        }
+        
+    }
+}
